806310cb391f11e517c8e34b0cf50c4b132d1013
ferencballa <f.balla.contact@gmail.com>
2021-12-07 06:27:05 +0100

    q7

--- src/code/Question7.java
+++ src/code/Question7.java
@@ -6,18 +6,62 @@ import java.nio.file.Path;
 
 public class Question7 {
     public static void main(String[] args) throws IOException {
-        String[] input = Files.readAllLines(Path.of("src/input/Question7.txt")).toArray(new String[0]);
+        String[] inp = Files.readAllLines(Path.of("src/input/Question7.txt")).toArray(new String[0]);
+        int[] input = Helper.StringArrayToInt(inp[0].split(","));
         System.out.println("Part 1:");
         part1(input);
         System.out.println("Part 2:");
         part2(input);
     }
 
-    private static void part1(String[] input) {
-
+    private static void part1(int[] input) {
+        int smallest = 100;
+        int biggest = 100;
+        for (int inp : input) {
+            if (inp < smallest) {
+                smallest = inp;
+            }
+            if (inp > biggest) {
+                biggest = inp;
+            }
+        }
+        int smallestDiff = Integer.MAX_VALUE;
+        int smallestIndex = -1;
+        for (int i = smallest; i <= biggest; i++) {
+            int diff = 0;
+            for (int inp : input) {
+                diff += Math.abs(i - inp);
+            }
+            if (diff < smallestDiff) {
+                smallestDiff = diff;
+            }
+        }
+        System.out.println(smallestDiff);
     }
 
-    private static void part2(String[] input) {
-
+    private static void part2(int[] input) {
+        int smallest = 100;
+        int biggest = 100;
+        for (int inp : input) {
+            if (inp < smallest) {
+                smallest = inp;
+            }
+            if (inp > biggest) {
+                biggest = inp;
+            }
+        }
+        int smallestDiff = Integer.MAX_VALUE;
+        int smallestIndex = -1;
+        for (int i = smallest; i <= biggest; i++) {
+            int diff = 0;
+            for (int inp : input) {
+                double x = Math.abs(i - inp);
+                diff += (x/2) * (x+1);
+            }
+            if (diff < smallestDiff) {
+                smallestDiff = diff;
+            }
+        }
+        System.out.println(smallestDiff);
     }
 }
