e9376bf946dda142df874e157a09604116ee9e95
ferencballa <f.balla.contact@gmail.com>
2021-12-12 20:36:26 +0100

    2019 q1-q5

--- src/year2019/code/Question1.java
+++ src/year2019/code/Question1.java
@@ -1,4 +1,6 @@
 package year2019.code;
+import helpers.Helper;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,10 +15,26 @@ public class Question1 {
     }
 
     private static void part1(String[] input) {
-
+        int[] values = Helper.StringArrayToInt(input);
+        int total = 0;
+        for (int val : values) {
+            total += Math.floorDiv(val, 3) -2;
+        }
+        System.out.println(total);
     }
 
     private static void part2(String[] input) {
-
+        int[] values = Helper.StringArrayToInt(input);
+        int total = 0;
+        for (int val : values) {
+            while (val > 0) {
+                int next = Math.floorDiv(val, 3) - 2;
+                if (next > 0) {
+                    total += next;
+                }
+                val = next;
+            }
+        }
+        System.out.println(total);
     }
 }
--- src/year2019/code/Question2.java
+++ src/year2019/code/Question2.java
@@ -1,4 +1,6 @@
 package year2019.code;
+import helpers.Helper;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,10 +15,54 @@ public class Question2 {
     }
 
     private static void part1(String[] input) {
-
+        String[] stringValues = input[0].split(",");
+        int[] values = Helper.StringArrayToInt(stringValues);
+        values[1] = 12;
+        values[2] = 2;
+        int curPos = 0;
+        while (values[curPos] != 99) {
+            if (values[curPos] == 1) {
+                values[values[curPos + 3]] = values[values[curPos + 1]] + values[values[curPos + 2]];
+            } else if (values[curPos] == 2) {
+                values[values[curPos + 3]] = values[values[curPos + 1]] * values[values[curPos + 2]];
+            } else {
+                System.out.println("Something went wrong");
+            }
+            curPos += 4;
+        }
+        System.out.println(values[0]);
     }
 
     private static void part2(String[] input) {
-
+        String[] stringValues = input[0].split(",");
+        int[] vals = Helper.StringArrayToInt(stringValues);
+        int noun = -1;
+        int verb = -1;
+        totalLoop:
+        for (int i = 0; i < 100; i++) {
+            noun = i;
+            for (int j = 0; j < 100; j++) {
+                int[] values = new int[vals.length];
+                System.arraycopy(vals, 0, values, 0, vals.length);
+                verb = j;
+                values[1] = i;
+                values[2] = j;
+                int curPos = 0;
+                while (values[curPos] != 99) {
+                    if (values[curPos] == 1) {
+                        values[values[curPos + 3]] = values[values[curPos + 1]] + values[values[curPos + 2]];
+                    } else if (values[curPos] == 2) {
+                        values[values[curPos + 3]] = values[values[curPos + 1]] * values[values[curPos + 2]];
+                    } else {
+                        System.out.println("Something went wrong");
+                    }
+                    curPos += 4;
+                }
+                if (values[0] == 19690720) {
+                    break totalLoop;
+                }
+            }
+        }
+        System.out.println(100 * noun + verb);
     }
 }
