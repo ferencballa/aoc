ce1acd55207b833e4691ebaeb222d39c1941ad8c
ferencballa <f.balla.contact@gmail.com>
2021-12-15 21:25:43 +0100

    optimized q15

--- src/year2021/code/Question15.java
+++ src/year2021/code/Question15.java
@@ -22,22 +22,22 @@ public class Question15 {
     }
 
     private static void part1(int[][] distances) {
-        HashMap<String, ArrayList<String>> neighbours = new HashMap<>();
-        HashMap<String, String> shortestPrev = new HashMap<>();
-        shortestPrev.put("0,0", "0,0:0");
+        HashMap<String, ArrayList<PointWithDistance>> neighbours = new HashMap<>();
+        HashMap<String, PointWithDistance> shortestPrev = new HashMap<>();
+        shortestPrev.put("0,0", new PointWithDistance("0,0", 0));
         for (int i = 0; i < distances.length; i++) {
             for (int j = 0; j < distances[0].length; j++) {
                 if (i != 0 || j != 0) {
-                    shortestPrev.put(i + "," + j, "NaN,NaN:" + Integer.MAX_VALUE);
+                    shortestPrev.put(i + "," + j, new PointWithDistance("NaN,NaN", Integer.MAX_VALUE));
                 }
-                ArrayList<String> neighboursOfPoint = new ArrayList<>();
+                ArrayList<PointWithDistance> neighboursOfPoint = new ArrayList<>();
                 for (int di = -1; di <= 1; di++) {
                     for (int dj = -1; dj <= 1; dj++) {
                         if ((di == 0 && dj != 0) || (di != 0 && dj == 0)) {
                             int newI = i + di;
                             int newJ = j + dj;
                             if (newI >= 0 && newI < distances.length && newJ >= 0 && newJ < distances[0].length) {
-                                neighboursOfPoint.add(newI + "," + newJ + ":" + distances[newI][newJ]);
+                                neighboursOfPoint.add(new PointWithDistance(newI + "," + newJ, distances[newI][newJ]));
                             }
                         }
                     }
@@ -48,26 +48,40 @@ public class Question15 {
         HashSet<String> checked = new HashSet<>();
         checked.add("0,0");
         ArrayList<String> unchecked = new ArrayList<>();
-        unchecked.add("1,0");
-        unchecked.add("0,1");
-        shortestPrev.put("1,0", "0,0:" + distances[1][0]);
-        shortestPrev.put("0,1", "0,0:" + distances[0][1]);
+        HashSet<String> uncheckedHash = new HashSet<>();
+        if (distances[1][0] > distances[0][1]) {
+            unchecked.add("0,1");
+            unchecked.add("1,0");
+        } else {
+            unchecked.add("1,0");
+            unchecked.add("0,1");
+        }
+        uncheckedHash.add("0,1");
+        uncheckedHash.add("1,0");
+        shortestPrev.put("1,0", new PointWithDistance("0,0", distances[1][0]));
+        shortestPrev.put("0,1", new PointWithDistance("0,0", distances[0][1]));
         while (!unchecked.isEmpty()) {
-            unchecked.sort((str1, str2) -> {
-                int d1 = Integer.parseInt(shortestPrev.get(str1).split(":")[1]);
-                int d2 = Integer.parseInt(shortestPrev.get(str2).split(":")[1]);
-                return d1 - d2;
-            });
             String cur = unchecked.remove(0);
-            for (String neighbour : neighbours.get(cur)) {
-                String[] neighbourFields = neighbour.split(":");
-                if (!checked.contains(neighbourFields[0])) {
-                    int newDistance = Integer.parseInt(shortestPrev.get(cur).split(":")[1]) + Integer.parseInt(neighbourFields[1]);
-                    if (Integer.parseInt(shortestPrev.get(neighbourFields[0]).split(":")[1]) > newDistance) {
-                        shortestPrev.put(neighbourFields[0], cur + ":" + newDistance);
+            uncheckedHash.remove(cur);
+            for (PointWithDistance neighbour : neighbours.get(cur)) {
+                if (!checked.contains(neighbour.point)) {
+                    int newDistance = shortestPrev.get(cur).distance + neighbour.distance;
+                    if (shortestPrev.get(neighbour.point).distance > newDistance) {
+                        shortestPrev.put(neighbour.point, new PointWithDistance(cur, newDistance));
                     }
-                    if (!unchecked.contains(neighbourFields[0])) {
-                        unchecked.add(neighbourFields[0]);
+                    if (!uncheckedHash.contains(neighbour.point)) {
+                        if (unchecked.isEmpty()) {
+                            unchecked.add(neighbour.point);
+                        } else {
+                            int d1 = shortestPrev.get(neighbour.point).distance;
+                            int i = 0;
+                            int uncheckedSize = unchecked.size();
+                            while (i < uncheckedSize && d1 > shortestPrev.get(unchecked.get(i)).distance) {
+                                i++;
+                            }
+                            unchecked.add(i, neighbour.point);
+                        }
+                        uncheckedHash.add(neighbour.point);
                     }
                 }
             }
@@ -76,15 +90,16 @@ public class Question15 {
         }
         int maxX = distances.length - 1;
         int maxY = distances[0].length - 1;
-        System.out.println(shortestPrev.get(maxX + "," + maxY).split(":")[1]);
+        System.out.println(shortestPrev.get(maxX + "," + maxY).distance);
     }
 
     private static void part2(int[][] distancesSmall) {
-        int[][] distances = new int[distancesSmall.length * 5][distancesSmall[0].length * 5];
+        int factor = 5;
+        int[][] distances = new int[distancesSmall.length * factor][distancesSmall[0].length * factor];
         for (int i = 0; i < distancesSmall.length; i++) {
             for (int j = 0; j < distancesSmall[0].length; j++) {
-                for (int di = 0; di < 5; di++) {
-                    for (int dj = 0; dj < 5; dj++) {
+                for (int di = 0; di < factor; di++) {
+                    for (int dj = 0; dj < factor; dj++) {
                         int newDistance = distancesSmall[i][j] + di + dj;
                         while (newDistance > 9) {
                             newDistance -= 9;
@@ -95,5 +110,15 @@ public class Question15 {
             }
         }
         part1(distances);
+    }
+}
+
+class PointWithDistance {
+    String point;
+    int distance;
+
+    PointWithDistance(String p, int d) {
+        point = p;
+        distance = d;
     }
 }
