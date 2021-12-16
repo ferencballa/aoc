f8a4378d8ca16e760747ca25a2685da35a2dc54e
ferencballa <f.balla.contact@gmail.com>
2021-12-16 14:51:01 +0100

    shortened code for q16

--- src/year2021/code/Question16.java
+++ src/year2021/code/Question16.java
@@ -25,109 +25,79 @@ public class Question16 {
     private static void part1() {
         countVersions += input[0] * 4 + input[1] * 2 + input[2];
         int outerType = input[3] * 4 + input[4] * 2 + input[5];
-        if (outerType == 4) {
-            unpackLiteral(6);
-        } else {
-            unpackOperator(6, outerType);
-        }
+        unpack(6, outerType);
         System.out.println(countVersions);
     }
 
     private static void part2() {
         int outerType = input[3] * 4 + input[4] * 2 + input[5];
         long total;
-        if (outerType == 4) {
-            total = (long)unpackLiteral(6).y;
-        } else {
-            total = (long)unpackOperator(6, outerType).y;
-        }
+        total = (long) unpack(6, outerType).y;
         System.out.println(total);
-
     }
 
-    private static Point2D.Double unpackLiteral(int index) {
-        ArrayList<Integer> literalBits = new ArrayList<>();
-        do {
-            literalBits.add(input[index + 1]);
-            literalBits.add(input[index + 2]);
-            literalBits.add(input[index + 3]);
-            literalBits.add(input[index + 4]);
-            index += 5;
-        } while (input[index - 5] == 1);
-        long value = 0;
-        int size = literalBits.size();
-        for (int i = 0; i < size; i++) {
-            value += literalBits.get(i) * Math.pow(2, size - 1 - i);
-        }
-        return new Point2D.Double(index, value);
-    }
-
-    private static Point2D.Double unpackOperator(int index, int type) {
-        ArrayList<Double> subValues = new ArrayList<>();
-        if (input[index] == 0) {
-            index++;
-            int length = 0;
-            for (int i = 0; i < 15; i++) {
-                length += input[index] * Math.pow(2, 14 - i);
-                index++;
+    private static Point2D.Double unpack(int index, int type) {
+        if (type == 4) {
+            ArrayList<Integer> literalBits = new ArrayList<>();
+            do {
+                literalBits.add(input[index + 1]);
+                literalBits.add(input[index + 2]);
+                literalBits.add(input[index + 3]);
+                literalBits.add(input[index + 4]);
+                index += 5;
+            } while (input[index - 5] == 1);
+            long value = 0;
+            int size = literalBits.size();
+            for (int i = 0; i < size; i++) {
+                value += literalBits.get(i) * Math.pow(2, size - 1 - i);
             }
-            int startIndex = index;
-            while (index < startIndex + length) {
-                countVersions += input[index] * 4 + input[index + 1] * 2 + input[index + 2];
-                int outerType = input[index + 3] * 4 + input[index + 4] * 2 + input[index + 5];
-                index += 6;
-                if (outerType == 4) {
-                    Point2D.Double res = unpackLiteral(index);
-                    index = (int)res.x;
-                    subValues.add(res.y);
-                } else {
-                    Point2D.Double res = unpackOperator(index, outerType);
-                    index = (int)res.x;
-                    subValues.add(res.y);
-                }
-            }
-            if (index > startIndex + length) {
-                System.out.println("Subs went on too long");
-            }
+            return new Point2D.Double(index, value);
         } else {
+            ArrayList<Double> subValues = new ArrayList<>();
             index++;
-            int numOfSubs = 0;
-            for (int i = 0; i < 11; i++) {
-                numOfSubs += input[index] * Math.pow(2, 10 - i);
-                index++;
-            }
-            for (int i = 0; i < numOfSubs; i++) {
-                countVersions += input[index] * 4 + input[index + 1] * 2 + input[index + 2];
-                int outerType = input[index + 3] * 4 + input[index + 4] * 2 + input[index + 5];
-                index += 6;
-                if (outerType == 4) {
-                    Point2D.Double res = unpackLiteral(index);
-                    index = (int)res.x;
+            if (input[index - 1] == 0) {
+                int length = 0;
+                for (int i = 0; i < 15; i++, index++) {
+                    length += input[index] * Math.pow(2, 14 - i);
+                }
+                int startIndex = index;
+                while (index < startIndex + length) {
+                    countVersions += input[index] * 4 + input[index + 1] * 2 + input[index + 2];
+                    int outerType = input[index + 3] * 4 + input[index + 4] * 2 + input[index + 5];
+                    Point2D.Double res = unpack(index + 6, outerType);
+                    index = (int) res.x;
                     subValues.add(res.y);
-                } else {
-                    Point2D.Double res = unpackOperator(index, outerType);
-                    index = (int)res.x;
+                }
+            } else {
+                int numOfSubs = 0;
+                for (int i = 0; i < 11; i++, index++) {
+                    numOfSubs += input[index] * Math.pow(2, 10 - i);
+                }
+                for (int i = 0; i < numOfSubs; i++) {
+                    countVersions += input[index] * 4 + input[index + 1] * 2 + input[index + 2];
+                    int outerType = input[index + 3] * 4 + input[index + 4] * 2 + input[index + 5];
+                    Point2D.Double res = unpack(index + 6, outerType);
+                    index = (int) res.x;
                     subValues.add(res.y);
                 }
             }
+            switch (type) {
+                case 0:
+                    return new Point2D.Double(index, subValues.stream().reduce(0.0, Double::sum));
+                case 1:
+                    return new Point2D.Double(index, subValues.stream().reduce(1.0, (a, b) -> a * b));
+                case 2:
+                    return new Point2D.Double(index, Collections.min(subValues));
+                case 3:
+                    return new Point2D.Double(index, Collections.max(subValues));
+                case 5:
+                    return new Point2D.Double(index, subValues.get(0) > subValues.get(1) ? 1 : 0);
+                case 6:
+                    return new Point2D.Double(index, subValues.get(0) < subValues.get(1) ? 1 : 0);
+                case 7:
+                    return new Point2D.Double(index, subValues.get(0).equals(subValues.get(1)) ? 1 : 0);
+            }
         }
-        switch (type) {
-            case 0:
-                return new Point2D.Double(index, subValues.stream().reduce(0.0, Double::sum));
-            case 1:
-                return new Point2D.Double(index, subValues.stream().reduce(1.0, (a, b) -> a * b));
-            case 2:
-                return new Point2D.Double(index, Collections.min(subValues));
-            case 3:
-                return new Point2D.Double(index, Collections.max(subValues));
-            case 5:
-                return new Point2D.Double(index, subValues.get(0) > subValues.get(1) ? 1 : 0);
-            case 6:
-                return new Point2D.Double(index, subValues.get(0) < subValues.get(1) ? 1 : 0);
-            case 7:
-                return new Point2D.Double(index, subValues.get(0).equals(subValues.get(1)) ? 1 : 0);
-        }
-        System.out.println("Unrecognized type");
-        return new Point2D.Double(index, -1);
+        return null;
     }
 }
