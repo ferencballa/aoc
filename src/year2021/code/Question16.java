02e2095bfe33c755259dc2ffb4aff03fc05a7bf0
ferencballa <f.balla.contact@gmail.com>
2021-12-16 14:26:14 +0100

    shortened code for q16

--- src/year2021/code/Question16.java
+++ src/year2021/code/Question16.java
@@ -1,6 +1,10 @@
 package year2021.code;
 
+import helpers.Helper;
+
+import java.awt.geom.Point2D;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -12,63 +16,8 @@ public class Question16 {
 
     public static void main(String[] args) throws IOException {
         String[] in = Files.readAllLines(Path.of("src/year2021/input/Question16.txt")).toArray(new String[0]);
-        String[] inp = in[0].split("");
-        input = new int[inp.length * 4];
-        for (int i = 0; i < inp.length; i++) {
-            int one = 0;
-            int two = 0;
-            int three = 0;
-            int four = 0;
-            switch(inp[i]) {
-                case "F" :
-                    one = 1;
-                case "7":
-                    two = 1;
-                case "3":
-                    three = 1;
-                case "1":
-                    four = 1;
-                    break;
-                case "E":
-                    one = 1;
-                case "6":
-                    two = 1;
-                case "2":
-                    three = 1;
-                    break;
-                case "D":
-                    one = 1;
-                case "5":
-                    two = 1;
-                    four = 1;
-                    break;
-                case "C":
-                    one = 1;
-                case "4":
-                    two = 1;
-                    break;
-                case "B":
-                    one = 1;
-                    three = 1;
-                    four = 1;
-                    break;
-                case "A":
-                    one = 1;
-                    three = 1;
-                    break;
-                case "9":
-                    one = 1;
-                    four = 1;
-                    break;
-                case "8":
-                    one = 1;
-                    break;
-            }
-            input[i*4] = one;
-            input[i*4+1] = two;
-            input[i*4+2] = three;
-            input[i*4+3] = four;
-        }
+        String binary = new BigInteger(in[0], 16).toString(2);
+        input = Helper.StringArrayToInt((new String(new char[in[0].length() * 4 - binary.length()]).replace('\0', '0') + binary).split(""));
         System.out.println("Part 1:");
         part1();
         System.out.println("Part 2:");
@@ -90,15 +39,15 @@ public class Question16 {
         int outerType = input[3] * 4 + input[4] * 2 + input[5];
         long total;
         if (outerType == 4) {
-            total = unpackLiteral(6).value;
+            total = (long)unpackLiteral(6).y;
         } else {
-            total = unpackOperator(6, outerType).value;
+            total = (long)unpackOperator(6, outerType).y;
         }
         System.out.println(total);
 
     }
 
-    private static IndexAndValue unpackLiteral(int index) {
+    private static Point2D.Double unpackLiteral(int index) {
         ArrayList<Integer> literalBits = new ArrayList<>();
         while (input[index] == 1) {
             literalBits.add(input[index + 1]);
@@ -117,11 +66,11 @@ public class Question16 {
         for (int i = 0; i < size; i++) {
             value += literalBits.get(i) * Math.pow(2, size - 1 - i);
         }
-        return new IndexAndValue(index, value);
+        return new Point2D.Double(index, value);
     }
 
-    private static IndexAndValue unpackOperator(int index, int type) {
-        ArrayList<Long> subValues = new ArrayList<>();
+    private static Point2D.Double unpackOperator(int index, int type) {
+        ArrayList<Double> subValues = new ArrayList<>();
         if (input[index] == 0) {
             index++;
             int length = 0;
@@ -135,13 +84,13 @@ public class Question16 {
                 int outerType = input[index + 3] * 4 + input[index + 4] * 2 + input[index + 5];
                 index += 6;
                 if (outerType == 4) {
-                    IndexAndValue res = unpackLiteral(index);
-                    index = res.index;
-                    subValues.add(res.value);
+                    Point2D.Double res = unpackLiteral(index);
+                    index = (int)res.x;
+                    subValues.add(res.y);
                 } else {
-                    IndexAndValue res = unpackOperator(index, outerType);
-                    index = res.index;
-                    subValues.add(res.value);
+                    Point2D.Double res = unpackOperator(index, outerType);
+                    index = (int)res.x;
+                    subValues.add(res.y);
                 }
             }
             if (index > startIndex + length) {
@@ -159,51 +108,41 @@ public class Question16 {
                 int outerType = input[index + 3] * 4 + input[index + 4] * 2 + input[index + 5];
                 index += 6;
                 if (outerType == 4) {
-                    IndexAndValue res = unpackLiteral(index);
-                    index = res.index;
-                    subValues.add(res.value);
+                    Point2D.Double res = unpackLiteral(index);
+                    index = (int)res.x;
+                    subValues.add(res.y);
                 } else {
-                    IndexAndValue res = unpackOperator(index, outerType);
-                    index = res.index;
-                    subValues.add(res.value);
+                    Point2D.Double res = unpackOperator(index, outerType);
+                    index = (int)res.x;
+                    subValues.add(res.y);
                 }
             }
         }
         switch (type) {
             case 0:
                 long sum = 0;
-                for (Long subValue : subValues) {
+                for (Double subValue : subValues) {
                     sum += subValue;
                 }
-                return new IndexAndValue(index, sum);
+                return new Point2D.Double(index, sum);
             case 1:
                 long product = 1;
-                for (Long subValue : subValues) {
+                for (Double subValue : subValues) {
                     product *= subValue;
                 }
-                return new IndexAndValue(index, product);
+                return new Point2D.Double(index, product);
             case 2:
-                return new IndexAndValue(index, Collections.min(subValues));
+                return new Point2D.Double(index, Collections.min(subValues));
             case 3:
-                return new IndexAndValue(index, Collections.max(subValues));
+                return new Point2D.Double(index, Collections.max(subValues));
             case 5:
-                return new IndexAndValue(index, subValues.get(0) > subValues.get(1) ? 1 : 0);
+                return new Point2D.Double(index, subValues.get(0) > subValues.get(1) ? 1 : 0);
             case 6:
-                return new IndexAndValue(index, subValues.get(0) < subValues.get(1) ? 1 : 0);
+                return new Point2D.Double(index, subValues.get(0) < subValues.get(1) ? 1 : 0);
             case 7:
-                return new IndexAndValue(index, subValues.get(0).equals(subValues.get(1)) ? 1 : 0);
+                return new Point2D.Double(index, subValues.get(0).equals(subValues.get(1)) ? 1 : 0);
         }
         System.out.println("Unrecognized type");
-        return new IndexAndValue(index, -1);
-    }
-}
-
-class IndexAndValue {
-    int index;
-    long value;
-
-    public IndexAndValue(int i, long v) {
-        index = i;
-        value = v;
+        return new Point2D.Double(index, -1);
     }
 }
