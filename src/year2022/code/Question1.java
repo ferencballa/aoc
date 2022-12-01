b6eb1c18a408c638c70e93c5f0d70582845bf674
Ferenc <f.balla.contact@gmail.com>
2022-12-01 21:34:51 +0100

    Fixed minor bug for Year 2022 day 1, added "single line" solution for fun

--- src/year2022/code/Question1.java
+++ src/year2022/code/Question1.java
@@ -3,8 +3,8 @@ package year2022.code;
 import helpers.Helper;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Question1 {
     public static void main(String[] args) throws IOException {
@@ -61,8 +61,35 @@ class Q1Part2 {
                 curTot += Integer.parseInt(currentValue);
             }
         }
+        cals.add(curTot);
         Collections.sort(cals);
         Collections.reverse(cals);
         System.out.println(cals.get(0) + cals.get(1) + cals.get(2));
+        System.out.println("1 line function:");
+        singleLineSolution(input);
+    }
+
+    private static void singleLineSolution(String[] input) {
+        System.out.println(Arrays.stream(
+                        Arrays.stream(input)
+                                .map(value -> value.equals("") ? " " : value)
+                                .collect(Collectors.joining(","))
+                                .split(" "))
+                .map(values ->
+                        Arrays.stream(values.split(","))
+                                .filter(val -> !val.equals(""))
+                                .map(Integer::parseInt)
+                                .reduce(0, Integer::sum))
+                .sorted()
+                .skip(Arrays.stream(
+                                Arrays.stream(input)
+                                        .map(value -> value.equals("") ? " " : value)
+                                        .collect(Collectors.joining(","))
+                                        .split(" "))
+                        .map(values ->
+                                Arrays.stream(values.split(","))
+                                        .reduce("", String::join))
+                        .count() - 3)
+                .reduce(0, Integer::sum));
     }
 }
