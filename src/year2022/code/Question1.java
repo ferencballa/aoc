afba579b8d5f07c24840aa97b135102690a28bb6
Ferenc <f.balla.contact@gmail.com>
2022-12-01 06:08:42 +0100

    Year 2022 day 1

--- src/year2022/code/Question1.java
+++ src/year2022/code/Question1.java
@@ -3,6 +3,8 @@ package year2022.code;
 import helpers.Helper;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Question1 {
     public static void main(String[] args) throws IOException {
@@ -23,6 +25,20 @@ class Q1Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question1.getInput();
+        int mostV = 0;
+        int curTot = 0;
+        for (int i = 0; i < input.length; i++) {
+            String currentValue = input[i];
+            if (currentValue.equals("")) {
+                if (curTot > mostV) {
+                    mostV = curTot;
+                }
+                curTot = 0;
+            } else {
+                curTot += Integer.parseInt(currentValue);
+            }
+        }
+        System.out.println(mostV);
     }
 }
 
@@ -34,5 +50,19 @@ class Q1Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question1.getInput();
+        ArrayList<Integer> cals = new ArrayList<>();
+        int curTot = 0;
+        for (int i = 0; i < input.length; i++) {
+            String currentValue = input[i];
+            if (currentValue.equals("")) {
+                cals.add(curTot);
+                curTot = 0;
+            } else {
+                curTot += Integer.parseInt(currentValue);
+            }
+        }
+        Collections.sort(cals);
+        Collections.reverse(cals);
+        System.out.println(cals.get(0) + cals.get(1) + cals.get(2));
     }
 }
