c35a0904ba3e3c25bcf29a520dac243f1560a59a
Ferenc <f.balla.contact@gmail.com>
2022-12-03 06:35:41 +0100

    Year 2022 day 3

--- src/year2022/code/Question3.java
+++ src/year2022/code/Question3.java
@@ -3,6 +3,7 @@ package year2022.code;
 import helpers.Helper;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Question3 {
     public static void main(String[] args) throws IOException {
@@ -23,6 +24,35 @@ class Q3Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question3.getInput();
+        int[][] inputsAsInts = new int[input.length][];
+        for (int i = 0; i < input.length; i++) {
+            inputsAsInts[i] = new int[input[i].length()];
+            for (int j = 0; j < inputsAsInts[i].length; j++) {
+                int val = (int) input[i].charAt(j);
+                if (val >= 97) {
+                    inputsAsInts[i][j] = val - 96;
+                } else {
+                    inputsAsInts[i][j] = val - 38;
+                }
+            }
+        }
+        int totalPrios = 0;
+        for (int i = 0; i < input.length; i++) {
+            int totalLineLength = inputsAsInts[i].length;
+            HashMap<Integer, Integer> numberInLeft = new HashMap<>();
+            for (int j = 0; j < totalLineLength/2; j++) {
+                numberInLeft.put(inputsAsInts[i][j], 1);
+            }
+            boolean badValFound = false;
+            for (int j = totalLineLength/2; j < totalLineLength; j++) {
+                Integer numberInLeftAmount = numberInLeft.get(inputsAsInts[i][j]);
+                if (!badValFound && numberInLeftAmount != null) {
+                    totalPrios += inputsAsInts[i][j];
+                    badValFound = true;
+                }
+            }
+        }
+        System.out.println(totalPrios);
     }
 }
 
@@ -34,5 +64,41 @@ class Q3Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question3.getInput();
+        int[][] inputsAsInts = new int[input.length][];
+        for (int i = 0; i < input.length; i++) {
+            inputsAsInts[i] = new int[input[i].length()];
+            for (int j = 0; j < inputsAsInts[i].length; j++) {
+                int val = (int) input[i].charAt(j);
+                if (val >= 97) {
+                    inputsAsInts[i][j] = val - 96;
+                } else {
+                    inputsAsInts[i][j] = val - 38;
+                }
+            }
+        }
+        int totalPrios = 0;
+        for (int i = 0; i < input.length; i+=3) {
+            int totalLineLength1 = inputsAsInts[i].length;
+            HashMap<Integer, Integer> numberInLeft1 = new HashMap<>();
+            for (int j = 0; j < totalLineLength1; j++) {
+                numberInLeft1.put(inputsAsInts[i][j], 1);
+            }
+            int totalLineLength2 = inputsAsInts[i+1].length;
+            HashMap<Integer, Integer> numberInLeft2 = new HashMap<>();
+            for (int j = 0; j < totalLineLength2; j++) {
+                numberInLeft2.put(inputsAsInts[i+1][j], 1);
+            }
+            boolean badValFound = false;
+            int totalLineLength3 = inputsAsInts[i+2].length;
+            for (int j = 0; j < totalLineLength3; j++) {
+                Integer numberInLeftAmount1 = numberInLeft1.get(inputsAsInts[i+2][j]);
+                Integer numberInLeftAmount2 = numberInLeft2.get(inputsAsInts[i+2][j]);
+                if (!badValFound && numberInLeftAmount1 != null && numberInLeftAmount2 != null) {
+                    totalPrios += inputsAsInts[i+2][j];
+                    badValFound = true;
+                }
+            }
+        }
+        System.out.println(totalPrios);
     }
 }
