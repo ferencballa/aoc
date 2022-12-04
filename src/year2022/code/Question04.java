83613a81f7138e1ea6bc5f9a1c0cf1f212480e2d
Ferenc <f.balla.contact@gmail.com>
2022-12-04 06:22:57 +0100

    Year 2022 day 04

--- src/year2022/code/Question04.java
+++ src/year2022/code/Question04.java
@@ -23,7 +23,20 @@ class Q04Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question04.getInput();
-
+        int overlap = 0;
+        for (int i = 0; i < input.length; i++) {
+            String[] lineParts = input[i].split(",");
+            String[] left = lineParts[0].split("-");
+            String[] right = lineParts[1].split("-");
+            int leftLow = Integer.parseInt(left[0]);
+            int leftHigh = Integer.parseInt(left[1]);
+            int rightLow = Integer.parseInt(right[0]);
+            int rightHigh = Integer.parseInt(right[1]);
+            if ((leftLow <= rightLow && leftHigh >= rightHigh || (rightLow <= leftLow && rightHigh >= leftHigh))) {
+                overlap++;
+            }
+        }
+        System.out.println(overlap);
     }
 }
 
@@ -35,5 +48,26 @@ class Q04Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question04.getInput();
+        int overlap = 0;
+        for (int i = 0; i < input.length; i++) {
+            String[] lineParts = input[i].split(",");
+            String[] left = lineParts[0].split("-");
+            String[] right = lineParts[1].split("-");
+            int leftLow = Integer.parseInt(left[0]);
+            int leftHigh = Integer.parseInt(left[1]);
+            int rightLow = Integer.parseInt(right[0]);
+            int rightHigh = Integer.parseInt(right[1]);
+            if (
+                    leftLow == rightLow ||
+                    leftLow == rightHigh ||
+                    leftHigh == rightLow ||
+                    leftHigh == rightHigh ||
+                    (leftLow < rightLow && leftHigh >= rightLow) ||
+                    (rightLow < leftLow && rightHigh >= leftLow)
+            ) {
+                overlap++;
+            }
+        }
+        System.out.println(overlap);
     }
 }
