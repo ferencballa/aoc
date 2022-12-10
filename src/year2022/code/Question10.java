aa2b4af126050687ee89e17d1841ac8df8cbd84c
Ferenc <f.balla.contact@gmail.com>
2022-12-10 15:23:20 +0100

    Year 2022 day 10 Added custom input

--- src/year2022/code/Question10.java
+++ src/year2022/code/Question10.java
@@ -3,15 +3,24 @@ package year2022.code;
 import helpers.Helper;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Question10 {
     public static void main(String[] args) throws IOException {
         Q10Part1.run();
         Q10Part2.run();
+        printNavara();
     }
 
     static String[] getInput() throws IOException {
         return Helper.getInputForYearAndTask(2022, 10);
+    }
+
+    static void printNavara() throws IOException {
+        String[] lines = Files.readAllLines(Path.of("src/year2022/input/Question10_NAVARA.txt")).toArray(new String[0]);
+        System.out.println("Printing NAVARA:");
+        Q10Part2.part2Algo(lines);
     }
 }
 
@@ -58,17 +67,20 @@ class Q10Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question10.getInput();
+        part2Algo(input);
+    }
+
+    static void part2Algo(String[] input) {
         String[] output = new String[240];
-        int signalStrength = 0;
         boolean wait = false;
         int inputIndex = 0;
         int x = 1;
         int bufferingAddition = 0;
         for (int cycle = 0; cycle < 240; cycle++) {
             if (x == cycle % 40 || x - 1 == cycle % 40 || x + 1 == cycle % 40) {
-                output[cycle] = "#";
+                output[cycle] = "█";
             } else {
-                output[cycle] = ".";
+                output[cycle] = " ";
             }
             if (!wait) {
                 if (input[inputIndex].equals("noop")) {
