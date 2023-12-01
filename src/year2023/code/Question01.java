26c0cf3395c1d138f9380f920c1e392822e4e36f
Ferenc <f.balla.contact@gmail.com>
2023-12-01 14:11:50 +0100

    cleanup Year 2023 day 1

--- src/year2023/code/Question01.java
+++ src/year2023/code/Question01.java
@@ -51,74 +51,52 @@ class Q01Part2 {
         int count = 0;
         for (String s : input) {
             int i = 0;
-            while (!(s.charAt(i) <= 57 && s.charAt(i) >= 48)
-            && !s.substring(i, Math.min(i + 3, s.length())).contains("one")
-                    && !s.substring(i, Math.min(i + 3, s.length())).contains("two")
-                    && !s.substring(i, Math.min(i + 5, s.length())).contains("three")
-                    && !s.substring(i, Math.min(i + 4, s.length())).contains("four")
-                    && !s.substring(i, Math.min(i + 4, s.length())).contains("five")
-                    && !s.substring(i, Math.min(i + 3, s.length())).contains("six")
-                    && !s.substring(i, Math.min(i + 5, s.length())).contains("seven")
-                    && !s.substring(i, Math.min(i + 5, s.length())).contains("eight")
-                    && !s.substring(i, Math.min(i + 4, s.length())).contains("nine")) {
+            while(true) {
+                int found = checkForNum(s, i, 10);
+                if (found != -1) {
+                    count += found; break;
+                }
                 i++;
             }
-            if (s.substring(i, Math.min(i + 3, s.length())).contains("one")) {
-                count += 10;
-            } else if (s.substring(i, Math.min(i + 3, s.length())).contains("two")) {
-                count += 20;
-            } else if (s.substring(i, Math.min(i + 5, s.length())).contains("three")) {
-                count += 30;
-            } else if (s.substring(i, Math.min(i + 4, s.length())).contains("four")) {
-                count += 40;
-            } else if (s.substring(i, Math.min(i + 4, s.length())).contains("five")) {
-                count += 50;
-            } else if (s.substring(i, Math.min(i + 3, s.length())).contains("six")) {
-                count += 60;
-            } else if (s.substring(i, Math.min(i + 5, s.length())).contains("seven")) {
-                count += 70;
-            } else if (s.substring(i, Math.min(i + 5, s.length())).contains("eight")) {
-                count += 80;
-            } else if (s.substring(i, Math.min(i + 4, s.length())).contains("nine")) {
-                count += 90;
-            } else {
-                count += (s.charAt(i) - 48) * 10;
-            }
             i = s.length() - 1;
-            while (!(s.charAt(i) <= 57 && s.charAt(i) >= 48)
-                    && !s.substring(i, Math.min(i + 3, s.length())).contains("one")
-                    && !s.substring(i, Math.min(i + 3, s.length())).contains("two")
-                    && !s.substring(i, Math.min(i + 5, s.length())).contains("three")
-                    && !s.substring(i, Math.min(i + 4, s.length())).contains("four")
-                    && !s.substring(i, Math.min(i + 4, s.length())).contains("five")
-                    && !s.substring(i, Math.min(i + 3, s.length())).contains("six")
-                    && !s.substring(i, Math.min(i + 5, s.length())).contains("seven")
-                    && !s.substring(i, Math.min(i + 5, s.length())).contains("eight")
-                    && !s.substring(i, Math.min(i + 4, s.length())).contains("nine")) {
+            while(true) {
+                int found = checkForNum(s, i, 1);
+                if (found != -1) {
+                    count += found; break;
+                }
                 i--;
-            }
-            if (s.substring(i, Math.min(i + 3, s.length())).contains("one")) {
-                count += 1;
-            } else if (s.substring(i, Math.min(i + 3, s.length())).contains("two")) {
-                count += 2;
-            } else if (s.substring(i, Math.min(i + 5, s.length())).contains("three")) {
-                count += 3;
-            } else if (s.substring(i, Math.min(i + 4, s.length())).contains("four")) {
-                count += 4;
-            } else if (s.substring(i, Math.min(i + 4, s.length())).contains("five")) {
-                count += 5;
-            } else if (s.substring(i, Math.min(i + 3, s.length())).contains("six")) {
-                count += 6;
-            } else if (s.substring(i, Math.min(i + 5, s.length())).contains("seven")) {
-                count += 7;
-            } else if (s.substring(i, Math.min(i + 5, s.length())).contains("eight")) {
-                count += 8;
-            } else if (s.substring(i, Math.min(i + 4, s.length())).contains("nine")) {
-                count += 9;
-            } else {
-                count += (s.charAt(i) - 48);
             }
         }
         System.out.println(count);
+    }
+
+    private static int checkForNum(String s, int i, int factor) {
+        if (ssOrEndEq(s, i, 3, "one")) {
+            return factor;
+        } else if (ssOrEndEq(s, i, 3, "two")) {
+            return 2 * factor;
+        } else if (ssOrEndEq(s, i, 5, "three")) {
+            return 3 * factor;
+        } else if (ssOrEndEq(s, i, 4, "four")) {
+            return 4 * factor;
+        } else if (ssOrEndEq(s, i, 4, "five")) {
+            return 5 * factor;
+        } else if (ssOrEndEq(s, i, 3, "six")) {
+            return 6 * factor;
+        } else if (ssOrEndEq(s, i, 5, "seven")) {
+            return 7 * factor;
+        } else if (ssOrEndEq(s, i, 5, "eight")) {
+            return 8 * factor;
+        } else if (ssOrEndEq(s, i, 4, "nine")) {
+            return 9 * factor;
+        } else if (s.charAt(i) <= 57 && s.charAt(i) >= 48) {
+            return (s.charAt(i) - 48) * factor;
+        }
+        return -1;
+    }
+
+    //also known as substringOrUntilEndEqualsString
+    private static boolean ssOrEndEq(String s, int i, int l, String comp) {
+        return s.substring(i, Math.min(i + l, s.length())).equals(comp);
     }
 }
