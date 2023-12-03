f831df029c7583a9b8964f2ba2cad314b71bed21
Ferenc <f.balla.contact@gmail.com>
2023-12-03 07:02:03 +0100

    Year 2023 day 2 & 3

--- src/year2023/code/Question02.java
+++ src/year2023/code/Question02.java
@@ -23,6 +23,24 @@ class Q02Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question02.getInput();
+        int count = 0;
+        for (String s : input) {
+            String[] parts = s.split(" ");
+            int i = 2;
+            boolean impossible = false;
+            while (i < parts.length - 1) {
+                if ((parts[i+1].startsWith("red") && Integer.parseInt(parts[i]) > 12) ||
+                        (parts[i+1].startsWith("green") && Integer.parseInt(parts[i]) > 13) ||
+                        (parts[i+1].startsWith("blue") && Integer.parseInt(parts[i]) > 14)) {
+                    impossible = true;
+                }
+                i += 2;
+            }
+            if (!impossible) {
+                count += Integer.parseInt(parts[1].substring(0, parts[1].length()-1));
+            }
+        }
+        System.out.println(count);
     }
 }
 
@@ -34,5 +52,27 @@ class Q02Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question02.getInput();
+        int count = 0;
+        for (String s : input) {
+            String[] parts = s.split(" ");
+            int i = 2;
+            int minRed = 0;
+            int minGreen = 0;
+            int minBlue = 0;
+            while (i < parts.length - 1) {
+                if (parts[i+1].startsWith("red")) {
+                    minRed = Math.max(Integer.parseInt(parts[i]), minRed);
+                }
+                else if (parts[i+1].startsWith("green")) {
+                    minGreen = Math.max(Integer.parseInt(parts[i]), minGreen);
+                }
+                else {
+                    minBlue = Math.max(Integer.parseInt(parts[i]), minBlue);
+                }
+                i += 2;
+            }
+            count += minRed * minGreen * minBlue;
+        }
+        System.out.println(count);
     }
 }
--- src/year2023/code/Question03.java
+++ src/year2023/code/Question03.java
@@ -2,7 +2,10 @@ package year2023.code;
 
 import helpers.Helper;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Question03 {
     public static void main(String[] args) throws IOException {
@@ -23,6 +26,58 @@ class Q03Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question03.getInput();
+        int count = 0;
+        for (int lineNumber = 0; lineNumber < input.length; lineNumber++) {
+            String s = input[lineNumber];
+            int i = 0;
+            while (i < s.length()) {
+                if (s.charAt(i) > 57 || s.charAt(i) < 48) {
+                    i++;
+                } else {
+                    int startOfNumber = i;
+                    while (i < s.length() && s.charAt(i) <= 57 && s.charAt(i) >= 48) {
+                        i++;
+                    }
+                    boolean isPartNumber = false;
+                    if (lineNumber != 0) {
+                        if (startOfNumber != 0) {
+                            isPartNumber = isPartNumber || isPartNumber(input, startOfNumber - 1, lineNumber - 1);
+                        }
+                        for (int z = startOfNumber; z < i; z++) {
+                            isPartNumber = isPartNumber || isPartNumber(input, z, lineNumber - 1);
+                        }
+                        if (i < s.length()) {
+                            isPartNumber = isPartNumber || isPartNumber(input, i, lineNumber - 1);
+                        }
+                    }
+                    if (startOfNumber != 0) {
+                        isPartNumber = isPartNumber || isPartNumber(input, startOfNumber - 1, lineNumber);
+                    }
+                    if (i < s.length()) {
+                        isPartNumber = isPartNumber || isPartNumber(input, i, lineNumber);
+                    }
+                    if (lineNumber != s.length() - 1) {
+                        if (startOfNumber != 0) {
+                            isPartNumber = isPartNumber || isPartNumber(input, startOfNumber - 1, lineNumber + 1);
+                        }
+                        for (int z = startOfNumber; z < i; z++) {
+                            isPartNumber = isPartNumber || isPartNumber(input, z, lineNumber + 1);
+                        }
+                        if (i < s.length()) {
+                            isPartNumber = isPartNumber || isPartNumber(input, i, lineNumber + 1);
+                        }
+                    }
+                    if (isPartNumber) {
+                        count += Integer.parseInt(s.substring(startOfNumber, i));
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean isPartNumber(String[] input, int x, int y) {
+        return input[y].charAt(x) != '.' && (input[y].charAt(x) > 57 || input[y].charAt(x) < 48);
     }
 }
 
@@ -34,5 +89,100 @@ class Q03Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question03.getInput();
+        HashMap<Point, ArrayList<Point>> adjacentNumbers = new HashMap<>();
+        HashMap<Point, Integer> valuesForStartingPoints = new HashMap<>();
+        for (int lineNumber = 0; lineNumber < input.length; lineNumber++) {
+            String s = input[lineNumber];
+            int i = 0;
+            while (i < s.length()) {
+                if (s.charAt(i) > 57 || s.charAt(i) < 48) {
+                    i++;
+                } else {
+                    int startOfNumber = i;
+                    while (i < s.length() && s.charAt(i) <= 57 && s.charAt(i) >= 48) {
+                        i++;
+                    }
+                    if (lineNumber != 0) {
+                        if (startOfNumber != 0) {
+                            if (isPartNumber(input, startOfNumber - 1, lineNumber - 1)) {
+                                Point curP = new Point(startOfNumber - 1, lineNumber - 1);
+                                addNumberIfNotAlreadyPresent(adjacentNumbers, curP, startOfNumber, lineNumber);
+                            }
+                        }
+                        for (int z = startOfNumber; z < i; z++) {
+                            if (isPartNumber(input, z, lineNumber - 1)) {
+                                Point curP = new Point(z, lineNumber - 1);
+                                addNumberIfNotAlreadyPresent(adjacentNumbers, curP, startOfNumber, lineNumber);
+                            }
+                        }
+                        if (i < s.length()) {
+                            if (isPartNumber(input, i, lineNumber - 1)) {
+                                Point curP = new Point(i, lineNumber - 1);
+                                addNumberIfNotAlreadyPresent(adjacentNumbers, curP, startOfNumber, lineNumber);
+                            }
+                        }
+                    }
+                    if (startOfNumber != 0) {
+                        if (isPartNumber(input, startOfNumber - 1, lineNumber)) {
+                            Point curP = new Point(startOfNumber - 1, lineNumber);
+                            addNumberIfNotAlreadyPresent(adjacentNumbers, curP, startOfNumber, lineNumber);
+                        }
+                    }
+                    if (i < s.length()) {
+                        if (isPartNumber(input, i, lineNumber)) {
+                            Point curP = new Point(i, lineNumber);
+                            addNumberIfNotAlreadyPresent(adjacentNumbers, curP, startOfNumber, lineNumber);
+                        }
+                    }
+                    if (lineNumber != s.length() - 1) {
+                        if (startOfNumber != 0) {
+                            if (isPartNumber(input, startOfNumber - 1, lineNumber + 1)) {
+                                Point curP = new Point(startOfNumber - 1, lineNumber + 1);
+                                addNumberIfNotAlreadyPresent(adjacentNumbers, curP, startOfNumber, lineNumber);
+                            }
+                        }
+                        for (int z = startOfNumber; z < i; z++) {
+                            if (isPartNumber(input, z, lineNumber + 1)) {
+                                Point curP = new Point(z, lineNumber + 1);
+                                addNumberIfNotAlreadyPresent(adjacentNumbers, curP, startOfNumber, lineNumber);
+                            }
+                        }
+                        if (i < s.length()) {
+                            if (isPartNumber(input, i, lineNumber + 1)) {
+                                Point curP = new Point(i, lineNumber + 1);
+                                addNumberIfNotAlreadyPresent(adjacentNumbers, curP, startOfNumber, lineNumber);
+                            }
+                        }
+                    }
+                    valuesForStartingPoints.put(new Point(startOfNumber, lineNumber), Integer.parseInt(s.substring(startOfNumber, i)));
+                }
+            }
+        }
+        int count = 0;
+        for (Point p : adjacentNumbers.keySet()) {
+            ArrayList<Point> points = adjacentNumbers.get(p);
+            if (points.size() == 2) {
+                count += valuesForStartingPoints.get(points.get(0)) * valuesForStartingPoints.get(points.get(1));
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean isPartNumber(String[] input, int x, int y) {
+        return input[y].charAt(x) != '.' && (input[y].charAt(x) > 57 || input[y].charAt(x) < 48);
+    }
+
+    private static void addNumberIfNotAlreadyPresent(HashMap<Point, ArrayList<Point>> adjacentNumbers, Point curP, int startOfNumber, int lineNumber) {
+        ArrayList<Point> numbersForThisPoint = adjacentNumbers.computeIfAbsent(curP, k -> new ArrayList<>());
+        boolean numberIsPresent = false;
+        for (Point p : numbersForThisPoint) {
+            if (p.x == startOfNumber && p.y == lineNumber) {
+                numberIsPresent = true;
+                break;
+            }
+        }
+        if (!numberIsPresent) {
+            numbersForThisPoint.add(new Point(startOfNumber, lineNumber));
+        }
     }
 }
