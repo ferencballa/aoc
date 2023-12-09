de89cbbbc67a6d72930484e8cb2b060caabcd43f
Ferenc <f.balla.contact@gmail.com>
2023-12-09 06:23:01 +0100

    Year 2023 day 9

--- src/year2023/code/Question09.java
+++ src/year2023/code/Question09.java
@@ -23,6 +23,37 @@ class Q09Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question09.getInput();
+        long[][] values = new long[input.length][];
+        long total = 0;
+        for (int i = 0; i < input.length; i++) {
+            String[] lineVals = input[i].split(" ");
+            values[i] = new long[lineVals.length];
+            for (int j = 0; j < lineVals.length; j++) {
+                values[i][j] = Long.parseLong(lineVals[j]);
+            }
+            total += calculateNextVal(values[i]);
+        }
+        System.out.println(total);
+    }
+
+    private static long calculateNextVal(long[] values) {
+        boolean allZeroes = true;
+        for (long val : values) {
+            if (val != 0) {
+                allZeroes = false;
+                break;
+            }
+        }
+        if (allZeroes) {
+            return 0;
+        } else {
+            long[] valuesDelta = new long[values.length-1];
+            for (int i = 0; i < valuesDelta.length; i++) {
+                valuesDelta[i] = values[i + 1] - values[i];
+            }
+            long deltaToNext = calculateNextVal(valuesDelta);
+            return values[values.length - 1] + deltaToNext;
+        }
     }
 }
 
@@ -34,5 +65,36 @@ class Q09Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question09.getInput();
+        long[][] values = new long[input.length][];
+        long total = 0;
+        for (int i = 0; i < input.length; i++) {
+            String[] lineVals = input[i].split(" ");
+            values[i] = new long[lineVals.length];
+            for (int j = 0; j < lineVals.length; j++) {
+                values[i][j] = Long.parseLong(lineVals[j]);
+            }
+            total += calculateNextVal(values[i]);
+        }
+        System.out.println(total);
+    }
+
+    private static long calculateNextVal(long[] values) {
+        boolean allZeroes = true;
+        for (long val : values) {
+            if (val != 0) {
+                allZeroes = false;
+                break;
+            }
+        }
+        if (allZeroes) {
+            return 0;
+        } else {
+            long[] valuesDelta = new long[values.length-1];
+            for (int i = 0; i < valuesDelta.length; i++) {
+                valuesDelta[i] = values[i + 1] - values[i];
+            }
+            long deltaToNext = calculateNextVal(valuesDelta);
+            return values[0] - deltaToNext;
+        }
     }
 }
