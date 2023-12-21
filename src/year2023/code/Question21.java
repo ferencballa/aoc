179b9a5b732db14656e803e7fc275dd485963a70
Ferenc <f.balla.contact@gmail.com>
2023-12-21 23:42:45 +0100

    Year 2023 day 21 updated with math in file

--- src/year2023/code/Question21.java
+++ src/year2023/code/Question21.java
@@ -76,17 +76,6 @@ class Q21Part1 {
 class Q21Part2 {
     public static void main(String[] args) throws IOException {
         run();
-        /*String pointString = "50,2148),(100,8690),(150,19370),(200,34831),(250,54119),(300,77770),(350,106229),(400,138362),(450,174872),(500,216540),(550,261791),(600,312217),(650,365887),(700,423800),(750,487364),(800,553972),(850,625268),(900,701764),(950,781035),(1000,866516),(1050,954620),(1100,1046766),(1150,1145567),(1200,1246086),(1250,1354122),(1300,1463392),(1350,1577487),(1400,1697864),(1450,1820046),(1500,1946802),(1550,2080621),(1600,2216270),(1650,2358654),(1700,2502509),(1750,2650692),(1800,2806456),(1850,2963116),(1900,3126463),(1950,3293265),(2000,3462847";
-        String[] pointStrings = pointString.split("\\),\\(");
-        Point[] points = new Point[pointStrings.length];
-        for (int i = 0; i < points.length; i++) {
-            String[] pointParts = pointStrings[i].split(",");
-            points[i] = new Point(Integer.parseInt(pointParts[0]), Integer.parseInt(pointParts[1]));
-        }
-        for (Point point : points) {
-            double diff = 0.866042 * point.x * point.x - 0.292172 * point.x + 74.7766 - point.y;
-            System.out.println(diff);
-        }*/
     }
 
     static void run() throws IOException {
@@ -100,8 +89,7 @@ class Q21Part2 {
             visitedSmall[i] = new boolean[input.length];
             stepsSmall[i] = new int[input.length];
         }
-        int offSetEachSide = 20;
-        //int offSetEachSide = 100;
+        int offSetEachSide = 4;
         int magnification = 1 + 2 * offSetEachSide;
         ArrayList<Pair<Point, Integer>> possibleSteps = new ArrayList<>();
         Point startPoint = new Point(-1, -1);
@@ -137,13 +125,7 @@ class Q21Part2 {
         int width = mapSmall.length;
         int middleToEdge = (mapSmall.length - 1) /2;
         int[] dataPoints = {middleToEdge, middleToEdge + width, middleToEdge + 2 * width};
-        //int[] dataPoints = {middleToEdge + 4 * width};
-        //int[] dataPoints = {537};
-        //int[] dataPoints = {6, 10, 50, 100, 500};
-        //int[] dataPoints = {1,3,5};
-        //int[] dataPoints = {100, 200, 300, 400, 500, 600, 700, 800};
         ArrayList<Point> resultPoints = new ArrayList<>();
-        String output = "plot points[";
         for (int dataPoint : dataPoints) {
             possibleSteps.add(new ImmutablePair<>(startPoint, 0));
             for (int i = 0; i < map.length; i++) {
@@ -191,9 +173,40 @@ class Q21Part2 {
                     }
                 }
             }
-            output += "(" + dataPoint + "," + count + "),";
             resultPoints.add(new Point(dataPoint, count));
         }
-        System.out.println(output.substring(0, output.length() - 1) + "]");
+        /*
+        y = ax^2 + bx + c
+
+        0,y1
+        1,y2
+        2,y3
+
+        y1 = a*0 + b*0 + c
+        y1 = c
+        c = y1
+        y2 = a*1 + b*1 + c
+        y2 = a + b + y1
+        y2 - y1 = a + b
+        y2 - y1 - b = a
+        y3 = a*4 + b*2 + c
+        y3 = 4a + 2b + y1
+        y3 - y1 = 4a + 2b
+        y3 - y1 = 4 (y2 - y1 - b) + 2b
+        y3 - y1 = 4y2 - 4y1 - 2b
+        2b = 4y2 - y3 - 3y1
+        b = 2y2 - 0.5y3 - 1.5y1
+        a = y2 - y1 - 2y2 + 0.5y3 + 1.5y1
+        a = 0.5y1 - y2 + 0.5y3
+         */
+        double y1 = resultPoints.get(0).y;
+        double y2 = resultPoints.get(1).y;
+        double y3 = resultPoints.get(2).y;
+        double a = 0.5 * y1 - y2 + 0.5 * y3;
+        double b = 2 * y2 - 0.5 * y3 - 1.5 * y1;
+        double c = y1;
+        double stepsToReach = 26501365;
+        double iterations = (stepsToReach - middleToEdge) / width;
+        System.out.printf("%.0f", ((long) a * iterations * iterations + b * iterations + c));
     }
 }
