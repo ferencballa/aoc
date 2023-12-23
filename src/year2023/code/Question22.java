66a0d13ebf17d8d00bd72e8f39328a88996afb67
Ferenc <f.balla.contact@gmail.com>
2023-12-23 18:33:25 +0100

    Year 2023 day 22 & 23

--- src/year2023/code/Question22.java
+++ src/year2023/code/Question22.java
@@ -1,8 +1,13 @@
 package year2023.code;
 
 import helpers.Helper;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Question22 {
     public static void main(String[] args) throws IOException {
@@ -23,6 +28,120 @@ class Q22Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question22.getInput();
+        int highestX = 0;
+        int highestY = 0;
+        int highestZ = 0;
+        ArrayList<Integer> orderedBrickIDs = new ArrayList<>();
+        HashMap<Integer, Pair<int[], int[]>> bricks = new HashMap<>();
+        for (int i = 0; i < input.length; i++) {
+            String line = input[i];
+            String[] endpoint = line.split("~");
+            String[] firstPoint = endpoint[0].split(",");
+            String[] secondPoint = endpoint[1].split(",");
+            int[] firstCoors = new int[3];
+            int[] secondCoors = new int[3];
+            firstCoors[0] = Integer.parseInt(firstPoint[0]);
+            firstCoors[1] = Integer.parseInt(firstPoint[1]);
+            firstCoors[2] = Integer.parseInt(firstPoint[2]);
+            secondCoors[0] = Integer.parseInt(secondPoint[0]);
+            secondCoors[1] = Integer.parseInt(secondPoint[1]);
+            secondCoors[2] = Integer.parseInt(secondPoint[2]);
+            highestX = Math.max(highestX, secondCoors[0]);
+            highestY = Math.max(highestY, secondCoors[1]);
+            highestZ = Math.max(highestZ, secondCoors[2]);
+            int posBrick = 0;
+            while (posBrick < orderedBrickIDs.size() && firstCoors[2] > bricks.get(orderedBrickIDs.get(posBrick)).getLeft()[2]) {
+                posBrick++;
+            }
+            orderedBrickIDs.add(posBrick, i + 1);
+            bricks.put(i + 1, new ImmutablePair<>(firstCoors, secondCoors));
+        }
+        int[][][] map = new int[highestX + 1][][];
+        for (int x = 0; x < map.length; x++) {
+            map[x] = new int[highestY + 1][];
+            for (int y = 0; y < map[x].length; y++) {
+                map[x][y] = new int[highestZ + 1];
+            }
+        }
+        for (int i = 0; i < orderedBrickIDs.size(); i++) {
+            int[] firstCoors = bricks.get(orderedBrickIDs.get(i)).getLeft();
+            int[] secondCoors = bricks.get(orderedBrickIDs.get(i)).getRight();
+            for (int x = firstCoors[0]; x <= secondCoors[0]; x++) {
+                for (int y = firstCoors[1]; y <= secondCoors[1]; y++) {
+                    for (int z = firstCoors[2]; z <= secondCoors[2]; z++) {
+                        map[x][y][z] = orderedBrickIDs.get(i);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < orderedBrickIDs.size(); i++) {
+            int brickID = orderedBrickIDs.remove(i);
+            Pair<int[], int[]> brick = bricks.get(brickID);
+            int[] firstCoors = brick.getLeft();
+            int[] secondCoors = brick.getRight();
+            goDown: while (true) {
+                if (firstCoors[2] <= 1) {
+                    break;
+                }
+                for (int x = firstCoors[0]; x <= secondCoors[0]; x++) {
+                    for (int y = firstCoors[1]; y <= secondCoors[1]; y++) {
+                        if (map[x][y][firstCoors[2] - 1] != 0) {
+                            break goDown;
+                        }
+                    }
+                }
+                for (int x = firstCoors[0]; x <= secondCoors[0]; x++) {
+                    for (int y = firstCoors[1]; y <= secondCoors[1]; y++) {
+                        if (x == -1 || y == -1 || firstCoors[2] - 1 == -1) {
+                            System.out.println("what");
+                        }
+                        map[x][y][firstCoors[2] - 1] = brickID;
+                        map[x][y][secondCoors[2]] = 0;
+                    }
+                }
+                firstCoors[2] = firstCoors[2] - 1;
+                secondCoors[2] = secondCoors[2] - 1;
+            }
+            bricks.put(brickID, new ImmutablePair<>(firstCoors, secondCoors));
+            orderedBrickIDs.add(i, brickID);
+        }
+        int count = 0;
+        for (Integer brickID : orderedBrickIDs) {
+            Pair<int[], int[]> brick = bricks.get(brickID);
+            int[] firstCoors = brick.getLeft();
+            int[] secondCoors = brick.getRight();
+            ArrayList<Integer> idsOfBricksItIsHolding = new ArrayList<>();
+            for (int x = firstCoors[0]; x <= secondCoors[0]; x++) {
+                for (int y = firstCoors[1]; y <= secondCoors[1]; y++) {
+                    if (map[x][y][secondCoors[2] + 1] != 0 && !idsOfBricksItIsHolding.contains(map[x][y][secondCoors[2] + 1])) {
+                        idsOfBricksItIsHolding.add(map[x][y][secondCoors[2] + 1]);
+                    }
+                }
+            }
+            boolean allBricksAreHeldBySomethingElse = true;
+            for (Integer heldBrickID : idsOfBricksItIsHolding) {
+                Pair<int[], int[]> heldBrick = bricks.get(heldBrickID);
+                int[] heldFirstCoors = heldBrick.getLeft();
+                int[] heldSecondCoors = heldBrick.getRight();
+                boolean thisBrickHeldBySomethingElse = false;
+                brickHeld: for (int x = heldFirstCoors[0]; x <= heldSecondCoors[0]; x++) {
+                    for (int y = heldFirstCoors[1]; y <= heldSecondCoors[1]; y++) {
+                        if (map[x][y][heldFirstCoors[2] - 1] != 0 && map[x][y][heldFirstCoors[2] - 1] != brickID) {
+                            thisBrickHeldBySomethingElse = true;
+                            break brickHeld;
+                        }
+                    }
+                }
+                if (!thisBrickHeldBySomethingElse) {
+                    allBricksAreHeldBySomethingElse = false;
+                    break;
+                }
+            }
+            if (allBricksAreHeldBySomethingElse) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
 
@@ -34,5 +153,128 @@ class Q22Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question22.getInput();
+        int highestX = 0;
+        int highestY = 0;
+        int highestZ = 0;
+        ArrayList<Integer> orderedBrickIDs = new ArrayList<>();
+        HashMap<Integer, Pair<int[], int[]>> bricks = new HashMap<>();
+        for (int i = 0; i < input.length; i++) {
+            String line = input[i];
+            String[] endpoint = line.split("~");
+            String[] firstPoint = endpoint[0].split(",");
+            String[] secondPoint = endpoint[1].split(",");
+            int[] firstCoors = new int[3];
+            int[] secondCoors = new int[3];
+            firstCoors[0] = Integer.parseInt(firstPoint[0]);
+            firstCoors[1] = Integer.parseInt(firstPoint[1]);
+            firstCoors[2] = Integer.parseInt(firstPoint[2]);
+            secondCoors[0] = Integer.parseInt(secondPoint[0]);
+            secondCoors[1] = Integer.parseInt(secondPoint[1]);
+            secondCoors[2] = Integer.parseInt(secondPoint[2]);
+            highestX = Math.max(highestX, secondCoors[0]);
+            highestY = Math.max(highestY, secondCoors[1]);
+            highestZ = Math.max(highestZ, secondCoors[2]);
+            int posBrick = 0;
+            while (posBrick < orderedBrickIDs.size() && firstCoors[2] > bricks.get(orderedBrickIDs.get(posBrick)).getLeft()[2]) {
+                posBrick++;
+            }
+            orderedBrickIDs.add(posBrick, i + 1);
+            bricks.put(i + 1, new ImmutablePair<>(firstCoors, secondCoors));
+        }
+        int[][][] map = new int[highestX + 1][][];
+        for (int x = 0; x < map.length; x++) {
+            map[x] = new int[highestY + 1][];
+            for (int y = 0; y < map[x].length; y++) {
+                map[x][y] = new int[highestZ + 1];
+            }
+        }
+        for (int i = 0; i < orderedBrickIDs.size(); i++) {
+            int[] firstCoors = bricks.get(orderedBrickIDs.get(i)).getLeft();
+            int[] secondCoors = bricks.get(orderedBrickIDs.get(i)).getRight();
+            for (int x = firstCoors[0]; x <= secondCoors[0]; x++) {
+                for (int y = firstCoors[1]; y <= secondCoors[1]; y++) {
+                    for (int z = firstCoors[2]; z <= secondCoors[2]; z++) {
+                        map[x][y][z] = orderedBrickIDs.get(i);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < orderedBrickIDs.size(); i++) {
+            int brickID = orderedBrickIDs.remove(i);
+            Pair<int[], int[]> brick = bricks.get(brickID);
+            int[] firstCoors = brick.getLeft();
+            int[] secondCoors = brick.getRight();
+            goDown: while (true) {
+                if (firstCoors[2] <= 1) {
+                    break;
+                }
+                for (int x = firstCoors[0]; x <= secondCoors[0]; x++) {
+                    for (int y = firstCoors[1]; y <= secondCoors[1]; y++) {
+                        if (map[x][y][firstCoors[2] - 1] != 0) {
+                            break goDown;
+                        }
+                    }
+                }
+                for (int x = firstCoors[0]; x <= secondCoors[0]; x++) {
+                    for (int y = firstCoors[1]; y <= secondCoors[1]; y++) {
+                        if (x == -1 || y == -1 || firstCoors[2] - 1 == -1) {
+                            System.out.println("what");
+                        }
+                        map[x][y][firstCoors[2] - 1] = brickID;
+                        map[x][y][secondCoors[2]] = 0;
+                    }
+                }
+                firstCoors[2] = firstCoors[2] - 1;
+                secondCoors[2] = secondCoors[2] - 1;
+            }
+            bricks.put(brickID, new ImmutablePair<>(firstCoors, secondCoors));
+            orderedBrickIDs.add(i, brickID);
+        }
+        int totalFall = 0;
+        for (Integer startBrickID : orderedBrickIDs) {
+            ArrayList<Integer> brickIDsToCheck = new ArrayList<>();
+            ArrayList<Integer> fallenBrickIDs = new ArrayList<>();
+            fallenBrickIDs.add(0);
+            brickIDsToCheck.add(startBrickID);
+            while (!brickIDsToCheck.isEmpty()) {
+                Integer brickID = brickIDsToCheck.remove(0);
+                Pair<int[], int[]> brick = bricks.get(brickID);
+                int[] firstCoors = brick.getLeft();
+                int[] secondCoors = brick.getRight();
+                boolean canFall = true;
+                canFallLoop: for (int x = firstCoors[0]; x <= secondCoors[0]; x++) {
+                    for (int y = firstCoors[1]; y <= secondCoors[1]; y++) {
+                        if (!fallenBrickIDs.contains(map[x][y][firstCoors[2] - 1])) {
+                            canFall = false;
+                            break canFallLoop;
+                        }
+                    }
+                }
+                if (canFall || Objects.equals(startBrickID, brickID)) {
+                    fallenBrickIDs.add(brickID);
+                    if (!Objects.equals(startBrickID, brickID)) {
+                        totalFall++;
+                    }
+                    ArrayList<Integer> idsOfBricksItIsHolding = new ArrayList<>();
+                    for (int x = firstCoors[0]; x <= secondCoors[0]; x++) {
+                        for (int y = firstCoors[1]; y <= secondCoors[1]; y++) {
+                            if (map[x][y][secondCoors[2] + 1] != 0 && !idsOfBricksItIsHolding.contains(map[x][y][secondCoors[2] + 1])) {
+                                idsOfBricksItIsHolding.add(map[x][y][secondCoors[2] + 1]);
+                            }
+                        }
+                    }
+                    for (Integer brickToAddToCheckID : idsOfBricksItIsHolding) {
+                        if (!brickIDsToCheck.contains(brickToAddToCheckID)) {
+                            int checkAddIndex = 0;
+                            while (checkAddIndex < brickIDsToCheck.size() && bricks.get(brickToAddToCheckID).getLeft()[2] > bricks.get(brickIDsToCheck.get(checkAddIndex)).getLeft()[2]) {
+                                checkAddIndex++;
+                            }
+                            brickIDsToCheck.add(checkAddIndex, brickToAddToCheckID);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(totalFall);
     }
 }
--- src/year2023/code/Question23.java
+++ src/year2023/code/Question23.java
@@ -1,8 +1,13 @@
 package year2023.code;
 
 import helpers.Helper;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Question23 {
     public static void main(String[] args) throws IOException {
@@ -23,6 +28,94 @@ class Q23Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question23.getInput();
+        char[][] map = new char[input[0].length()][];
+        for (int i = 0; i < map.length; i++) {
+            map[i] = new char[input.length];
+        }
+        for (int y = 0; y < input.length; y++) {
+            for (int x = 0; x < input[y].length(); x++) {
+                map[x][y] = input[y].charAt(x);
+            }
+        }
+        int longestRoute = 0;
+        ArrayList<Pair<Point, boolean[][]>> posAndVisitedQueue = new ArrayList<>();
+        HashMap<Pair<Point, boolean[][]>, Integer> distanceForPosAndVisisted = new HashMap<>();
+        boolean[][] startVisited = new boolean[map.length][];
+        for (int i = 0; i < startVisited.length; i++) {
+            startVisited[i] = new boolean[map[i].length];
+        }
+        startVisited[1][0] = true;
+        Pair<Point, boolean[][]> startPosAndVisited = new ImmutablePair<>(new Point(1, 0), startVisited);
+        posAndVisitedQueue.add(startPosAndVisited);
+        distanceForPosAndVisisted.put(startPosAndVisited, 0);
+        //debug list:
+        ArrayList<Integer> routeLengths = new ArrayList<>();
+        while (!posAndVisitedQueue.isEmpty()) {
+            Pair<Point, boolean[][]> posAndVisited = posAndVisitedQueue.remove(0);
+            Point coors = posAndVisited.getLeft();
+            int x = coors.x;
+            int y = coors.y;
+            boolean[][] visited = posAndVisited.getRight();
+            int distance = distanceForPosAndVisisted.get(posAndVisited);
+            boolean noSplit = true;
+            boolean noPath = false;
+            boolean reachedFinish = false;
+            while (noSplit && !noPath && !reachedFinish) {
+                distance++;
+                ArrayList<Pair<Point, boolean[][]>> nextPosAndVisitedList = new ArrayList<>();
+                if (y > 0 && !visited[x][y-1] && (map[x][y-1]=='.' || map[x][y-1]=='^')) {
+                    boolean[][] newVisited = new boolean[visited.length][];
+                    for (int i = 0; i < newVisited.length; i++) {
+                        newVisited[i] = visited[i].clone();
+                    }
+                    newVisited[x][y-1] = true;
+                    nextPosAndVisitedList.add(new ImmutablePair<>(new Point(x, y-1), newVisited));
+                }
+                if (x < map.length - 1 && !visited[x+1][y] && (map[x+1][y]=='.' || map[x+1][y]=='>')) {
+                    boolean[][] newVisited = new boolean[visited.length][];
+                    for (int i = 0; i < newVisited.length; i++) {
+                        newVisited[i] = visited[i].clone();
+                    }
+                    newVisited[x+1][y] = true;
+                    nextPosAndVisitedList.add(new ImmutablePair<>(new Point(x+1, y), newVisited));
+                }
+                if (y < map[0].length - 1 && !visited[x][y+1] && (map[x][y+1]=='.' || map[x][y+1]=='v')) {
+                    boolean[][] newVisited = new boolean[visited.length][];
+                    for (int i = 0; i < newVisited.length; i++) {
+                        newVisited[i] = visited[i].clone();
+                    }
+                    newVisited[x][y+1] = true;
+                    nextPosAndVisitedList.add(new ImmutablePair<>(new Point(x, y+1), newVisited));
+                }
+                if (x > 0 && !visited[x-1][y] && (map[x-1][y]=='.' || map[x-1][y]=='<')) {
+                    boolean[][] newVisited = new boolean[visited.length][];
+                    for (int i = 0; i < newVisited.length; i++) {
+                        newVisited[i] = visited[i].clone();
+                    }
+                    newVisited[x-1][y] = true;
+                    nextPosAndVisitedList.add(new ImmutablePair<>(new Point(x-1, y), newVisited));
+                }
+                if (nextPosAndVisitedList.size() == 0) {
+                    noPath = true;
+                } else if (nextPosAndVisitedList.size() > 1) {
+                    noSplit = false;
+                    for (Pair<Point, boolean[][]> nextPosAndVisited : nextPosAndVisitedList) {
+                        posAndVisitedQueue.add(nextPosAndVisited);
+                        distanceForPosAndVisisted.put(nextPosAndVisited, distance);
+                    }
+                } else if (nextPosAndVisitedList.get(0).getLeft().x == map.length - 2 && nextPosAndVisitedList.get(0).getLeft().y == map[0].length - 1) {
+                    reachedFinish = true;
+                    longestRoute = Math.max(longestRoute, distance);
+                    routeLengths.add(distance);
+                } else {
+                    visited = nextPosAndVisitedList.get(0).getRight();
+                    x = nextPosAndVisitedList.get(0).getLeft().x;
+                    y = nextPosAndVisitedList.get(0).getLeft().y;
+                }
+            }
+        }
+        System.out.println(routeLengths);
+        System.out.println(longestRoute);
     }
 }
 
@@ -34,5 +127,105 @@ class Q23Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question23.getInput();
+        char[][] map = new char[input[0].length()][];
+        for (int i = 0; i < map.length; i++) {
+            map[i] = new char[input.length];
+        }
+        for (int y = 0; y < input.length; y++) {
+            for (int x = 0; x < input[y].length(); x++) {
+                map[x][y] = input[y].charAt(x);
+            }
+        }
+        ArrayList<Point> nodes = new ArrayList<>();
+        nodes.add(new Point(1, 0));
+        nodes.add(new Point(map.length - 2, map[0].length - 1));
+        for (int x = 1; x < map.length - 1; x++) {
+            for (int y = 1; y < map.length - 1; y++) {
+                int walls = 0;
+                if (map[x-1][y] == '#') {
+                    walls++;
+                }
+                if (map[x][y+1] == '#') {
+                    walls++;
+                }
+                if (map[x+1][y] == '#') {
+                    walls++;
+                }
+                if (map[x][y-1] == '#') {
+                    walls++;
+                }
+                if (walls != 2) {
+                    nodes.add(new Point(x, y));
+                }
+            }
+        }
+        HashMap<Point, ArrayList<Pair<Point, Integer>>> pointsWithAdjacentNodes = new HashMap<>();
+        for (Point node : nodes) {
+            ArrayList<Point> startDirections = new ArrayList<>();
+            if (map[node.x-1][node.y] != '#') {
+                startDirections.add(new Point(node.x-1, node.y));
+            }
+            if (node.y < map[0].length - 1 && map[node.x][node.y+1] != '#') {
+                startDirections.add(new Point(node.x, node.y+1));
+            }
+            if (map[node.x+1][node.y] != '#') {
+                startDirections.add(new Point(node.x+1, node.y));
+            }
+            if (node.y > 0 && map[node.x][node.y-1] != '#') {
+                startDirections.add(new Point(node.x, node.y-1));
+            }
+            ArrayList<Pair<Point, Integer>> adjacentNodes = new ArrayList<>();
+            for (Point coor : startDirections) {
+                Point prev = new Point(node.x, node.y);
+                int distance = 0;
+                while (true) {
+                    distance++;
+                    if ((coor.x == 1 && coor.y == 0) || (coor.x == map.length - 2 && coor.y == map[0].length - 1)) {
+                        break;
+                    }
+                    ArrayList<Point> possibilities = new ArrayList<>();
+                    if (!(coor.x-1 == prev.x && coor.y == prev.y) && map[coor.x-1][coor.y] != '#') {
+                        possibilities.add(new Point(coor.x-1, coor.y));
+                    }
+                    if (!(coor.x == prev.x && coor.y+1 == prev.y) && map[coor.x][coor.y+1] != '#') {
+                        possibilities.add(new Point(coor.x, coor.y+1));
+                    }
+                    if (!(coor.x+1 == prev.x && coor.y == prev.y) && map[coor.x+1][coor.y] != '#') {
+                        possibilities.add(new Point(coor.x+1, coor.y));
+                    }
+                    if (!(coor.x == prev.x && coor.y-1 == prev.y) && map[coor.x][coor.y-1] != '#') {
+                        possibilities.add(new Point(coor.x, coor.y-1));
+                    }
+                    if (possibilities.size() > 1) {
+                        break;
+                    } else {
+                        prev = new Point(coor.x, coor.y);
+                        coor = new Point(possibilities.get(0).x, possibilities.get(0).y);
+                    }
+                }
+                adjacentNodes.add(new ImmutablePair<>(coor, distance));
+            }
+            pointsWithAdjacentNodes.put(node, adjacentNodes);
+        }
+        ArrayList<Point> startVisited = new ArrayList<>();
+        startVisited.add(new Point(1, 0));
+        System.out.println(searchForEnd(new Point(1, 0), 0, pointsWithAdjacentNodes, startVisited, map.length - 2, map[0].length - 1));
+    }
+
+    private static int searchForEnd(Point currentNode, int currentDistance, HashMap<Point, ArrayList<Pair<Point, Integer>>> pointsWithAdjacentNodes, ArrayList<Point> visitedNodes, int finalX, int finalY) {
+        if (currentNode.x == finalX && currentNode.y == finalY) {
+            return currentDistance;
+        } else {
+            ArrayList<Pair<Point, Integer>> adjacentNodes = pointsWithAdjacentNodes.get(currentNode);
+            int longestDistance = 0;
+            for (Pair<Point, Integer> nodeWithDistance : adjacentNodes) {
+                if (!visitedNodes.contains(nodeWithDistance.getLeft())) {
+                    ArrayList<Point> newVisitedNodes = new ArrayList<>(visitedNodes);
+                    newVisitedNodes.add(nodeWithDistance.getLeft());
+                    longestDistance = Math.max(longestDistance, searchForEnd(nodeWithDistance.getLeft(), currentDistance + nodeWithDistance.getRight(), pointsWithAdjacentNodes, newVisitedNodes, finalX, finalY));
+                }
+            }
+            return longestDistance;
+        }
     }
 }
