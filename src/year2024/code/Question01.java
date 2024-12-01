c2b503afa0fe6fd5627aa8acccaa81f9c9b32d8f
Ferenc <f.balla.contact@gmail.com>
2024-12-01 06:13:26 +0100

    Solution day 1 year 2024

--- src/year2024/code/Question01.java
+++ src/year2024/code/Question01.java
@@ -3,6 +3,7 @@ package year2024.code;
 import helpers.Helper;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Question01 {
     public static void main(String[] args) throws IOException {
@@ -23,6 +24,21 @@ class Q01Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question01.getInput();
+        int[] list1 = new int[input.length];
+        int[] list2 = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            String s = input[i];
+            String[] parts = s.split("   ");
+            list1[i] = Integer.parseInt(parts[0]);
+            list2[i] = Integer.parseInt(parts[1]);
+        }
+        list1 = Arrays.stream(list1).sorted().toArray();
+        list2 = Arrays.stream(list2).sorted().toArray();
+        int sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            sum += Math.abs(list1[i] - list2[i]);
+        }
+        System.out.println(sum);
     }
 }
 
@@ -34,5 +50,28 @@ class Q01Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question01.getInput();
+        int[] list1 = new int[input.length];
+        int[] list2 = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            String s = input[i];
+            String[] parts = s.split("   ");
+            list1[i] = Integer.parseInt(parts[0]);
+            list2[i] = Integer.parseInt(parts[1]);
+        }
+        list1 = Arrays.stream(list1).sorted().toArray();
+        list2 = Arrays.stream(list2).sorted().toArray();
+        int sum = 0;
+        int index2 = 0;
+        for (int i = 0; i < input.length; i++) {
+            int count = 0;
+            while (index2 < input.length && list2[index2] <= list1[i]) {
+                if (list2[index2] == list1[i]) {
+                    count++;
+                }
+                index2++;
+            }
+            sum += list1[i] * count;
+        }
+        System.out.println(sum);
     }
 }
