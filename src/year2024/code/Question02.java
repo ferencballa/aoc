c387005c61cda749b16b437aaa746ccd49c3dd88
Ferenc <f.balla.contact@gmail.com>
2024-12-02 10:38:56 +0100

    Solution day 2 year 2024

--- src/year2024/code/Question02.java
+++ src/year2024/code/Question02.java
@@ -23,6 +23,32 @@ class Q02Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question02.getInput();
+        int count = 0;
+        for (String s : input) {
+            String[] vals = s.split(" ");
+            boolean increasing = true;
+            boolean decreasing = true;
+            int index = 0;
+            while ((increasing || decreasing) && index < vals.length - 1) {
+                int val1 = Integer.parseInt(vals[index]);
+                int val2 = Integer.parseInt(vals[index+1]);
+                if (val1 == val2 || Math.abs(val1 - val2) > 3) {
+                    increasing = false;
+                    decreasing = false;
+                }
+                if (val1 > val2) {
+                    increasing = false;
+                }
+                if (val1 < val2) {
+                    decreasing = false;
+                }
+                index++;
+            }
+            if (increasing || decreasing) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
 
@@ -34,5 +60,49 @@ class Q02Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question02.getInput();
+        int count = 0;
+        for (String s : input) {
+            String[] valsOriginal = s.split(" ");
+            boolean increasingAtAll = false;
+            boolean decreasingAtAll = false;
+            for (int i = 0; i <= valsOriginal.length; i++) {
+                String[] vals;
+                if (i < valsOriginal.length) {
+                    vals = new String[valsOriginal.length - 1];
+                    for (int ind = 0; ind < i; ind++) {
+                        vals[ind] = valsOriginal[ind];
+                    }
+                    for (int ind = i+1; ind < valsOriginal.length; ind++) {
+                        vals[ind-1] = valsOriginal[ind];
+                    }
+                } else {
+                    vals = valsOriginal;
+                }
+                boolean increasing = true;
+                boolean decreasing = true;
+                int index = 0;
+                while ((increasing || decreasing) && index < vals.length - 1) {
+                    int val1 = Integer.parseInt(vals[index]);
+                    int val2 = Integer.parseInt(vals[index + 1]);
+                    if (val1 == val2 || Math.abs(val1 - val2) > 3) {
+                        increasing = false;
+                        decreasing = false;
+                    }
+                    if (val1 > val2) {
+                        increasing = false;
+                    }
+                    if (val1 < val2) {
+                        decreasing = false;
+                    }
+                    index++;
+                }
+                increasingAtAll = increasingAtAll || increasing;
+                decreasingAtAll = decreasingAtAll || decreasing;
+            }
+            if (increasingAtAll || decreasingAtAll) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
