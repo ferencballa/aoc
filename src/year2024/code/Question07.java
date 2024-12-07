8d4a13db8a5a3e25cbaec8ed69822cf85af5fa0e
Ferenc <f.balla.contact@gmail.com>
2024-12-07 12:52:53 +0100

    Solution day 7 year 2024

--- src/year2024/code/Question07.java
+++ src/year2024/code/Question07.java
@@ -3,6 +3,7 @@ package year2024.code;
 import helpers.Helper;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Question07 {
     public static void main(String[] args) throws IOException {
@@ -23,6 +24,34 @@ class Q07Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question07.getInput();
+        long count = 0;
+        for (String line : input) {
+            String[] parts = line.split(": ");
+            long result = Long.parseLong(parts[0]);
+            ArrayList<Long> vals = Helper.StringArrayToLongArrayList(parts[1].split(" "));
+            long comb = vals.remove(0);
+            if (validResult(result, comb, (ArrayList<Long>) vals.clone(), true) ||validResult(result, comb, (ArrayList<Long>) vals.clone(), false)) {
+                count += result;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean validResult(long result, long comb, ArrayList<Long> vals, boolean add) {
+        if (vals.isEmpty()) {
+            return result == comb;
+        }
+        long next = vals.remove(0);
+        if (add) {
+            comb += next;
+        } else {
+            comb *= next;
+        }
+        if (comb > result) {
+            return false;
+        } else {
+            return validResult(result, comb, (ArrayList<Long>) vals.clone(), true) ||validResult(result, comb, (ArrayList<Long>) vals.clone(), false);
+        }
     }
 }
 
@@ -34,5 +63,35 @@ class Q07Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question07.getInput();
+        long count = 0;
+        for (String line : input) {
+            String[] parts = line.split(": ");
+            long result = Long.parseLong(parts[0]);
+            ArrayList<Long> vals = Helper.StringArrayToLongArrayList(parts[1].split(" "));
+            long comb = vals.remove(0);
+            if (validResult(result, comb, (ArrayList<Long>) vals.clone(), 0) ||validResult(result, comb, (ArrayList<Long>) vals.clone(), 1) ||validResult(result, comb, (ArrayList<Long>) vals.clone(), 2)) {
+                count += result;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean validResult(long result, long comb, ArrayList<Long> vals, int op) {
+        if (vals.isEmpty()) {
+            return result == comb;
+        }
+        long next = vals.remove(0);
+        if (op == 0) {
+            comb += next;
+        } else if (op == 1) {
+            comb *= next;
+        } else {
+            comb = Long.parseLong(Long.toString(comb).concat(Long.toString(next)));
+        }
+        if (comb > result) {
+            return false;
+        } else {
+            return validResult(result, comb, (ArrayList<Long>) vals.clone(), 0) ||validResult(result, comb, (ArrayList<Long>) vals.clone(), 1) ||validResult(result, comb, (ArrayList<Long>) vals.clone(), 2);
+        }
     }
 }
