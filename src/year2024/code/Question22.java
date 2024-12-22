1670ac9e6c3b6b952b58546ecb250b0301f2aacc
Ferenc <f.balla.contact@gmail.com>
2024-12-23 00:21:39 +0100

    Solution day 22 year 2024

--- src/year2024/code/Question22.java
+++ src/year2024/code/Question22.java
@@ -3,6 +3,7 @@ package year2024.code;
 import helpers.Helper;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Question22 {
     public static void main(String[] args) throws IOException {
@@ -23,6 +24,24 @@ class Q22Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question22.getInput();
+        int times = 2000;
+        long count = 0;
+        for (String s : input) {
+            long val = Long.parseLong(s);
+            for (int i = 0; i < times; i++) {
+                long mix = val * 64;
+                val ^= mix;
+                val %= 16777216;
+                mix = Math.floorDiv(val, 32);
+                val ^= mix;
+                val %= 16777216;
+                mix = val * 2048;
+                val ^= mix;
+                val %= 16777216;
+            }
+            count += val;
+        }
+        System.out.println(count);
     }
 }
 
@@ -34,5 +53,53 @@ class Q22Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question22.getInput();
+        int times = 2000;
+        HashMap<String, Long> map = new HashMap<>();
+        for (String s : input) {
+            HashMap<String, Long> mapForSeller = new HashMap<>();
+            long val = Long.parseLong(s);
+            long valPrev = val;
+            val = doIteration(val);
+            long diff1 = Long.parseLong(Long.toString(val).substring(Long.toString(val).length() - 1)) - Long.parseLong(Long.toString(valPrev).substring(Long.toString(valPrev).length() - 1));
+            valPrev = val;
+            val = doIteration(val);
+            long diff2 = Long.parseLong(Long.toString(val).substring(Long.toString(val).length() - 1)) - Long.parseLong(Long.toString(valPrev).substring(Long.toString(valPrev).length() - 1));
+            valPrev = val;
+            val = doIteration(val);
+            long diff3 = Long.parseLong(Long.toString(val).substring(Long.toString(val).length() - 1)) - Long.parseLong(Long.toString(valPrev).substring(Long.toString(valPrev).length() - 1));
+            for (int i = 3; i < times; i++) {
+                valPrev = val;
+                val = doIteration(val);
+                long diff4 = Long.parseLong(Long.toString(val).substring(Long.toString(val).length() - 1)) - Long.parseLong(Long.toString(valPrev).substring(Long.toString(valPrev).length() - 1));
+                String key = Long.toString(diff1) + diff2 + diff3 + diff4;
+                if (!mapForSeller.containsKey(key)) {
+                    mapForSeller.put(key, Long.parseLong(Long.toString(val).substring(Long.toString(val).length() - 1)));
+                }
+                diff1 = diff2;
+                diff2 = diff3;
+                diff3 = diff4;
+            }
+            for (String key : mapForSeller.keySet()) {
+                map.put(key, (map.containsKey(key) ? map.get(key) : 0) + mapForSeller.get(key));
+            }
+        }
+        long maxVal = 0;
+        for (String key : map.keySet()) {
+            maxVal = Math.max(maxVal, map.get(key));
+        }
+        System.out.println(maxVal);
+    }
+
+    private static long doIteration(Long val) {
+        long mix = val * 64;
+        val ^= mix;
+        val %= 16777216;
+        mix = Math.floorDiv(val, 32);
+        val ^= mix;
+        val %= 16777216;
+        mix = val * 2048;
+        val ^= mix;
+        val %= 16777216;
+        return val;
     }
 }
