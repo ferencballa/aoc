e16560be41cc5a3a1a0d3abf84866141225bbb69
Ferenc <f.balla.contact@gmail.com>
2025-12-04 12:47:01 +0100

    Solution day 4 year 2025

--- src/year2025/code/Question04.java
+++ src/year2025/code/Question04.java
@@ -23,6 +23,35 @@ class Q04Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question04.getInput();
+        boolean[][] grid = new boolean[input[0].length()][input.length];
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[0].length(); j++) {
+                grid[j][i] = input[i].charAt(j) == '@';
+            }
+        }
+        int count = 0;
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                if (grid[x][y]) {
+                    int neighbours = 0;
+                    for (int dx = -1; dx <= 1; dx++) {
+                        for (int dy = -1; dy <= 1; dy++) {
+                            if (!(dx == 0 && dy == 0)) {
+                                if (!(x + dx < 0 || y + dy < 0 || x + dx == grid[0].length || y + dy == grid.length)) {
+                                    if (grid[x + dx][y + dy]) {
+                                        neighbours++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (neighbours < 4) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
 
@@ -34,5 +63,40 @@ class Q04Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question04.getInput();
+        boolean[][] grid = new boolean[input[0].length()][input.length];
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[0].length(); j++) {
+                grid[j][i] = input[i].charAt(j) == '@';
+            }
+        }
+        int totalCount = 0;
+        int count = -1;
+        while (count != 0) {
+            count = 0;
+            for (int y = 0; y < grid.length; y++) {
+                for (int x = 0; x < grid[0].length; x++) {
+                    if (grid[x][y]) {
+                        int neighbours = 0;
+                        for (int dx = -1; dx <= 1; dx++) {
+                            for (int dy = -1; dy <= 1; dy++) {
+                                if (!(dx == 0 && dy == 0)) {
+                                    if (!(x + dx < 0 || y + dy < 0 || x + dx == grid[0].length || y + dy == grid.length)) {
+                                        if (grid[x + dx][y + dy]) {
+                                            neighbours++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (neighbours < 4) {
+                            count++;
+                            grid[x][y] = false;
+                        }
+                    }
+                }
+            }
+            totalCount += count;
+        }
+        System.out.println(totalCount);
     }
 }
