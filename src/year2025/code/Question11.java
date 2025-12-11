7abd84fe6d21a166f4dc00f0e6225a8ff4baadc9
Ferenc <f.balla.contact@gmail.com>
2025-12-11 16:19:51 +0100

    Solution day 11 year 2025

--- src/year2025/code/Question11.java
+++ src/year2025/code/Question11.java
@@ -3,6 +3,8 @@ package year2025.code;
 import helpers.Helper;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Question11 {
     public static void main(String[] args) throws IOException {
@@ -16,6 +18,9 @@ public class Question11 {
 }
 
 class Q11Part1 {
+
+    private static HashMap<String, ArrayList<String>> outsPerNode = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         run();
     }
@@ -23,10 +28,47 @@ class Q11Part1 {
     static void run() throws IOException {
         System.out.println("Part 1:");
         String[] input = Question11.getInput();
+        for (int i = 0; i < input.length; i++) {
+            String[] vals = input[i].split(" ");
+            ArrayList<String> outsForCur = new ArrayList<>();
+            for (int v = 1; v < vals.length; v++) {
+                outsForCur.add(vals[v]);
+            }
+            outsPerNode.put(vals[0].substring(0, vals[0].length() - 1), outsForCur);
+        }
+        System.out.println(countPathsToOut("you", new ArrayList<>()));
+    }
+
+    private static int countPathsToOut(String cur, ArrayList<String> prevNodes) {
+        if (cur.equals("out")) {
+            return 1;
+        }
+        int pathsToOut = 0;
+        ArrayList<String> nextNodes = outsPerNode.get(cur);
+        for (String node : nextNodes) {
+            boolean visited = false;
+            for (String prevNode : prevNodes) {
+                if (node.equals(prevNode) || node.equals(cur)) {
+                    visited = true;
+                    break;
+                }
+            }
+            if (!visited) {
+                ArrayList<String> newPrevNodes = (ArrayList<String>) prevNodes.clone();
+                newPrevNodes.add(cur);
+                pathsToOut += countPathsToOut(node, newPrevNodes);
+            }
+        }
+        return pathsToOut;
     }
 }
 
 class Q11Part2 {
+
+    private static HashMap<String, ArrayList<String>> outsPerNode = new HashMap<>();
+
+    private static HashMap<String, long[]> outsFurtherDown = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         run();
     }
@@ -34,5 +76,63 @@ class Q11Part2 {
     static void run() throws IOException {
         System.out.println("Part 2:");
         String[] input = Question11.getInput();
+        for (int i = 0; i < input.length; i++) {
+            String[] vals = input[i].split(" ");
+            ArrayList<String> outsForCur = new ArrayList<>();
+            for (int v = 1; v < vals.length; v++) {
+                outsForCur.add(vals[v]);
+            }
+            outsPerNode.put(vals[0].substring(0, vals[0].length() - 1), outsForCur);
+        }
+        System.out.println(countPathsToOut("svr", new ArrayList<>())[0]);
+    }
+
+    private static long[] countPathsToOut(String cur, ArrayList<String> prevNodes) {
+        if (cur.equals("out")) {
+            long[] ret = new long[4];
+            ret[3] = 1;
+            return ret;
+        }
+        long[] pathsToOut = new long[4];
+        ArrayList<String> nextNodes = outsPerNode.get(cur);
+        for (String node : nextNodes) {
+            boolean visited = false;
+            for (String prevNode : prevNodes) {
+                if (node.equals(prevNode) || node.equals(cur)) {
+                    visited = true;
+                    break;
+                }
+            }
+            if (!visited) {
+                if (outsFurtherDown.containsKey(node)) {
+                    pathsToOut[0] += outsFurtherDown.get(node)[0];
+                    pathsToOut[1] += outsFurtherDown.get(node)[1];
+                    pathsToOut[2] += outsFurtherDown.get(node)[2];
+                    pathsToOut[3] += outsFurtherDown.get(node)[3];
+                } else {
+                    ArrayList<String> newPrevNodes = (ArrayList<String>) prevNodes.clone();
+                    newPrevNodes.add(cur);
+                    long[] p = countPathsToOut(node, newPrevNodes);
+                    pathsToOut[0] += p[0];
+                    pathsToOut[1] += p[1];
+                    pathsToOut[2] += p[2];
+                    pathsToOut[3] += p[3];
+                    outsFurtherDown.put(node, p);
+                }
+            }
+        }
+        if (cur.equals("dac")) {
+            pathsToOut[1] += pathsToOut[3];
+            pathsToOut[0] += pathsToOut[2];
+            pathsToOut[3] = 0;
+            pathsToOut[2] = 0;
+        }
+        if (cur.equals("fft")) {
+            pathsToOut[2] += pathsToOut[3];
+            pathsToOut[0] += pathsToOut[1];
+            pathsToOut[3] = 0;
+            pathsToOut[1] = 0;
+        }
+        return pathsToOut;
     }
 }
